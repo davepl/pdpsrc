0317d6253df901cfe943d5a6d9e7cf6f833a1d50
Dave Plummer <davepl@davepl.com>
2025-07-26 18:05:20 -0700

    Inlcude header in byte count

--- bsd/socket/panel_packet.h
+++ bsd/socket/panel_packet.h
@@ -11,9 +11,11 @@ typedef unsigned short u16_t;   /* Always 16 bits */
 typedef unsigned long  u32_t;   /* 32 bits */
 
 /* Common packet header structure */
+
+#pragma pack(push, 1)
 struct panel_packet_header {
     u16_t pp_byte_count;    /* Size of the panel_state payload */
     u32_t pp_byte_flags;    /* Panel type flags (PANEL_PDP1170, PANEL_VAX, etc.) */
 };
-
+#pragma pack(pop)
 #endif /* PANEL_PACKET_H */
